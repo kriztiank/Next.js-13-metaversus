d1d3b44deb59b13319d462773ba88f673af572c5
kriztiank <kriztiank@gmail.com>
2023-03-28 10:36:01 +0200

    first commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,19 +1,19 @@
-'use client';
+'use client'
 
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 
-import styles from '../styles';
-import { navVariants } from '../utils/motion';
+import styles from '../styles'
+import { navVariants } from '../utils/motion'
 
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
-    initial="hidden"
-    whileInView="show"
+    initial='hidden'
+    whileInView='show'
     className={`${styles.xPaddings} py-8 relative`}
   >
     navbar
   </motion.nav>
-);
+)
 
-export default Navbar;
+export default Navbar
